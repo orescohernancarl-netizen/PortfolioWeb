396a37a1ce64b7ae7a91e7cfa6e8c6f54bc0729d
CaelWebDev <orescohernancarl@gmail.com>
2026-08-03 12:40:45 +0800

    update btn size

--- script.js
+++ script.js
@@ -336,31 +336,6 @@ window.addEventListener("scroll", () => {
   BUTTON RIPPLE EFFECT
 =====================================*/
 
-document.querySelectorAll(".btn").forEach(button => {
-    button.addEventListener("click", function (e) {
-
-        const oldRipple = this.querySelector(".ripple");
-        if (oldRipple) oldRipple.remove();
-
-        const ripple = document.createElement("span");
-        ripple.className = "ripple";
-
-        const size = Math.min(this.offsetWidth, this.offsetHeight);
-
-        ripple.style.width = size + "px";
-        ripple.style.height = size + "px";
-
-        const rect = this.getBoundingClientRect();
-
-        ripple.style.left = (e.clientX - rect.left - size / 2) + "px";
-        ripple.style.top = (e.clientY - rect.top - size / 2) + "px";
-
-        this.appendChild(ripple);
-
-        ripple.addEventListener("animationend", () => ripple.remove());
-
-    });
-});
 
 /*=====================================
   PARALLAX HERO EFFECT
